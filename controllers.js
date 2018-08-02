897448e35090a010b9729768d75ac8a18d9e9341
1997priyam <1997priyam@gmail.com>
2018-08-02 20:11:11 +0530

    Added my API key

--- controllers.js
+++ controllers.js
@@ -15,7 +15,7 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
     
     $scope.days = $routeParams.days || '2';
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/", { appid:'YourAPIKey' }, { get: { method: "GET" }});
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/", { appid:'5b8255240d4896e33d15452e647843f4' }, { get: { method: "GET" }});
     
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
